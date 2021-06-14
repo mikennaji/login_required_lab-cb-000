29d676cdc13bf6fea5bda7d1ee026179a4ed7511
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-14 01:13:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get '/' => 'secrets#show'
- get '/login' => 'sessions#new'
+ get '/new' => 'sessions#new'
  post '/login' => 'sessions#create'
  post '/logout' =>  'sessions#destroy'
 end
