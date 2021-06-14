5c1856d098e2bbaf59f37ce3de029732607128cd
Michael Nnaji <mnnaji3@uwo.ca>
2021-06-14 01:07:42 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,13 +1,13 @@
 class SecretsController < ApplicationController
   before_action :require_login
-  skip_action :require_login, only: [:show]
+  skip_after_action :require_login, only: [:show]
 
  def require_login
   return head(:forbidden) unless session.include? :user_name
  end
 
  def show
-
+   logged_in
 end
 
 
